df10e36e0491bfbe06266389fa13dc0aa47ead61
lastcountr <lastcountr@gmail.com>
2023-04-14 16:31:08 +0530

    Adding and linking second activity, and more user interface improvements, such as removing action bar and status bar

--- app/src/main/java/com/example/quizzi/MainActivity.kt
+++ app/src/main/java/com/example/quizzi/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.quizzi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startBtn = findViewById<Button>(R.id.start_btn)
+        val editName = findViewById<EditText>(R.id.edit_name)
+
+        startBtn.setOnClickListener {
+            if(editName.text.isEmpty()){
+                Toast.makeText(this, "Please enter yout name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, Questions::class.java)
+                startActivity(intent)
+                finish()
+
+
+            }
+        }
+
     }
 }
